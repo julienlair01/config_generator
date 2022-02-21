a80cdc1a538d385200e6feecfe3d97e6f21fe91f
julienlair01 <julien.lair@gmail.com>
2022-02-21 23:08:25 +0100

    first models

--- config_gen/catalog/admin.py
+++ config_gen/catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from catalog.models import Manufacturer, Station
 
 # Register your models here.
+admin.site.register(Manufacturer)
+admin.site.register(Station)
--- config_gen/catalog/models.py
+++ config_gen/catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Station(models.Model):
+    reference = models.CharField(max_length=200)
+    evse_count = models.IntegerField()
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.reference
+    
