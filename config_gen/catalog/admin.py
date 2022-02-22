df2c292309a5249243567034c683ee2a94dce1af
julienlair01 <julien.lair@gmail.com>
2022-02-22 22:13:20 +0100

    killed all

--- config_gen/catalog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from catalog.models import Manufacturer, Chargebox
-
-# Register your models here.
-admin.site.register(Manufacturer)
-admin.site.register(Chargebox)
